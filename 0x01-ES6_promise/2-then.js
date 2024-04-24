ca55d0be3a05efbc2994f47926f7d50703fb447a
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2024-04-24 16:23:39 +0000

    new

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromApi(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
-    .finally(() => console.log('Got a reponse from the API'));
+    .finally(() => console.log('Got a response from the API'));
 }

c00123b477b19e36ff2255bcb2b8ab26de5f819d
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-06-20 14:45:29 +0300

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -8,7 +8,7 @@ describe("sendPaymentRequestToApi", function() {
     it("check that Utils.calculateNumber was called once", function() {
         const spy = sinon.spy(Utils, "calculateNumber");
 
-        sendPaymentRequestToApi;(10, 20);
+        sendPaymentRequestToApi(100, 20);
 
         assert(spy.calledOnce);
         spy.restore();

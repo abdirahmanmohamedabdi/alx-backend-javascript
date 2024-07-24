55cc7a1adf40254d5bd0a31ca1f1a57947e980a3
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-07-24 21:10:19 +0300

    task new

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from './utils';
+
+const asyncuploader = async () => {
+  try {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+};
+export default asyncuploader;

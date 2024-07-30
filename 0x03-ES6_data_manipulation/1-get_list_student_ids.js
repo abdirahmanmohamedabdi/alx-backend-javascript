fce81dccd2b26aea4757de1db2711bcb58b9f2ac
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-07-30 11:32:32 +0300

    new

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+const getListStudentIds = (listStudents) => {
+  let listStudentsIds = [];
+  if (!(listStudents instanceof Array)) {
+    return listStudentsIds;
+  }
+
+  listStudentsIds = listStudents.map((student) => student.id);
+  return listStudentsIds;
+};
+
+export default getListStudentIds;
